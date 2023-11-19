bb54fb0b839c24d28643baf1398e5d621c8d2f9c
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-19 16:04:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 function firstWord(s) {
   // your code here
+
+	let j=-1;
+
+	let n=s.length;
+
+	for(let i=0;i<n;i++)
+		{
+			if(s[i]!=" ")
+			{
+				j=i;
+				break;
+			}
+		}
+
+	if(j==-1)
+		return s;
+    
+	let ss1="";
+
+	for(let i=j;i<n;i++)
+		{
+			if(s[i]==" ") 
+			{
+				break;
+			}
+			
+			ss1+=s[i];
+		}
+
+	return ss1;
+	
 }
 
-// Do not change the code below
+// Do not change the code below 
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
